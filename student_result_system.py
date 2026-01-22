07989188656c4017dc486374373be4d26324b02c
OLUWAFEZI9 <fezikid003@gmail.com>
2026-01-22 13:08:18 +0100

    Add files via upload

---
+++ student_result_system.py
@@ -0,0 +1,10 @@
+student_name = input("Enter student name: ")
+
+score1 = int(input("Enter first score: "))
+score2 = int(input("Enter second score: "))
+score3 = int(input("Enter third score: "))
+
+average = (score1 + score2 + score3) / 3
+
+print("Student Name:", student_name)
+print("Average Score:", average)
